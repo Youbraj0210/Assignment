ad911fa88f9317bc3cb43a63b9d2506b94c624c0
unknown <youbraj.singh>
2025-06-28 19:49:10 +0530

    Assignment Completed!

---
+++ TrafficSignalEnum/TrafficSignalEnum/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TrafficSignalEnum
+{
+    enum TrafficLight { RED, YELLOW, GREEN }
+
+
+    internal class Program
+    {
+        public static void TrafficActions(TrafficLight light)
+        {
+            switch (light)
+            {
+                case (TrafficLight.RED): Console.WriteLine("STOP!"); break;
+                case TrafficLight.YELLOW: Console.WriteLine("READY!"); break;
+                case TrafficLight.GREEN: Console.WriteLine("GO!"); break;
+                default: Console.WriteLine("invalid input!"); break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TrafficActions(TrafficLight.RED);
+            TrafficActions(TrafficLight.YELLOW);
+            TrafficActions(TrafficLight.GREEN);
+        }
+    }
+}
