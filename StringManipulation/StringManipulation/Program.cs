49197e1738feb85667f5c51980ea5bf573efd9b0
unknown <youbraj.singh>
2025-06-29 12:08:50 +0530

    Assignment Completed!

---
+++ StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StringManipulation
+{
+    internal class Program
+    {
+        public static void Seperate(string str)
+        {
+            string[] strings = str.Split(',');
+
+            Console.WriteLine("Developer name: " + strings[0]);
+            Console.WriteLine("Company name: " + strings[1]);
+            Console.WriteLine("Location name: " + strings[2]);
+        }
+        static void Main(string[] args)
+        {
+            Seperate("Sachin,Aurionrpo,Mumbai");
+        }
+    }
+}
