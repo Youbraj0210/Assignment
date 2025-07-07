e244504cf8ba1662a1ee36e630ab754685094d92
unknown <youbraj.singh>
2025-07-07 14:55:40 +0530

    Assignment COmpleted!

---
+++ StudentGrading/StudentGrading/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentGrading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("John Cena");
+            Student student2 = new Student("Randy Orton", 78);
+            Student student3 = new Student("Cody Rhodes", 99, 'A');
+
+            Student.AssignGrade(student1);
+            Student.AssignGrade(student2);
+            Student.AssignGrade(student3);
+
+            student1.DisplayInfo();
+            student2.DisplayInfo();
+            student3.DisplayInfo();
+        }
+    }
+}
---
+++ StudentGrading/StudentGrading/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGrading
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Marks { get; set; }
+        public char Grade { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public Student(string name, int marks)
+        {
+            Name = name;
+            Marks = marks;
+        }
+
+        public Student(string name, int marks, char grade)
+        {
+            Name = name;
+            Marks = marks;
+            Grade = grade;
+        }
+
+        public static void AssignGrade(Student obj)
+        {
+            if (obj.Marks >= 90)
+                obj.Grade = 'A';
+
+            if (obj.Marks >= 80 && obj.Marks < 90)
+                obj.Grade = 'B';
+
+            if (obj.Marks >= 70 && obj.Marks < 80)
+                obj.Grade = 'C';
+
+            if (obj.Marks < 70)
+                obj.Grade = 'D';
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name} Marks: {Marks} Grade: {Grade}");
+        }
+    }
+}
