5075c838b945c9508cb8e31509706b3c448c6ea6
unknown <youbraj.singh>
2025-07-07 23:42:34 +0530

    Assignment Completed!

---
+++ ReverseANumber/ReverseANumber/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ReverseANumber
+{
+    internal class Program
+    {
+        public static void ReverseNumber(int number)
+        {
+            int savedNumber = number;
+            int reverse = 0;
+            while (number > 0)
+            {
+                int temp = number % 10;
+                reverse = reverse * 10 + temp;
+                number /= 10;
+            }
+
+            Console.WriteLine(reverse);
+
+            if (savedNumber == reverse)
+            {
+                Console.WriteLine($"{savedNumber} is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{savedNumber} is not a Palindrome");
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            ReverseNumber(12334);
+        }
+    }
+}
