fb0e30351621144328fa635f1cf62acc6210af50
unknown <youbraj.singh>
2025-07-07 23:04:40 +0530

    Assignment Completed!

---
+++ DayOfTheWeek/DayOfTheWeek/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DayOfTheWeek
+{
+    enum DaysOfWeek { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY }
+
+    internal class Program
+    {
+
+        public static void Week(int num)
+        {
+            DaysOfWeek day = DaysOfWeek.SUNDAY;
+            switch (num)
+            {
+                case 1: day = DaysOfWeek.MONDAY; break;
+                case 2: day = DaysOfWeek.TUESDAY; break;
+                case 3: day = DaysOfWeek.WEDNESDAY; break;
+                case 4: day = DaysOfWeek.THURSDAY; break;
+                case 5: day = DaysOfWeek.FRIDAY; break;
+                case 6: day = DaysOfWeek.SATURDAY; break;
+                case 7: day = DaysOfWeek.SUNDAY; break;
+                default: Console.WriteLine("wrong output!"); break;
+            }
+            Console.WriteLine(day);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number (1-7): ");
+            Week(int.Parse(Console.ReadLine()));
+        }
+    }
+}
