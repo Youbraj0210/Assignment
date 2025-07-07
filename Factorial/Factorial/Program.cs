f6f5efb0f7441fba7bf8347138f76dd8db3051d1
unknown <youbraj.singh>
2025-07-07 23:39:26 +0530

    Assignment Completed!

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        public static int Factorial(int num)
+        {
+            int fact = 1;
+            while (num != 1)
+            {
+                fact *= num;
+                num--;
+            }
+
+            return fact;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));
+        }
+    }
+}
