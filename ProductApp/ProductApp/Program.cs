c88eea55654091c55f0412e2af6f5f0874f64959
unknown <youbraj.singh>
2025-07-08 21:51:57 +0530

    Assignment Completed!

---
+++ ProductApp/ProductApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product(1, "pencil", 10);
+            Product p2 = new Product(2, "pen", 50);
+
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p2.ToString());
+        }
+    }
+}
