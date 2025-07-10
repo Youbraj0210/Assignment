40bab51a9f0268110a52204d4b25a989b4613a22
unknown <youbraj.singh>
2025-07-10 16:25:21 +0530

    Assignment Completed!

---
+++ Countspaces/Countspaces/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Countspaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello How are you?";
+            int count = 0;
+
+            foreach(char c in str)
+            {
+                if(c == ' ')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Total space: "+count);
+        }
+    }
+}
