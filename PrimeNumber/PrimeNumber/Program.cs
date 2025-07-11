8da185a3a228bf1bae19627a5909dc51faa55669
unknown <youbraj.singh>
2025-07-11 15:38:49 +0530

    Assignment Completed!

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PrimeNumber
+{
+    internal class Program
+    {
+        public static bool IsPrime(int n)
+        {
+            bool isPrime = true;
+            for(int i = 2; i < n / 2; i++)
+            {
+                if(n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            return isPrime;
+        }
+        static void Main(string[] args)
+        {
+            int number = 23;
+            Console.WriteLine(IsPrime(number));
+        }
+    }
+}
