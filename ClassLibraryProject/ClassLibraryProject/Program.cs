532dca73e7f7008d0f81dd23470462b080c55012
unknown <youbraj.singh>
2025-07-11 14:15:44 +0530

    Assignment Completed!

---
+++ ClassLibraryProject/ClassLibraryProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using MyClassLibrary;
+
+namespace ClassLibraryProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* steps to create class library:
+             * create new project of type class library
+             * add the project reference in the main project
+             * and also add using namespace of the class library in the man project to use the classes 
+             */
+            Account account = new Account("HDFC123456" , "Youbraj" ,100000);
+            Console.WriteLine(account.ToString());
+        }
+    }
+}
---
+++ ClassLibraryProject/MyClassLibrary/Account.cs
@@ -0,0 +1,21 @@
+﻿namespace MyClassLibrary
+{
+    public class Account
+    {
+        public string AccountNumber { get; set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+
+        public Account(string accountNumber, string name, int balance)
+        {
+            AccountNumber = accountNumber;
+            Name = name;
+            Balance = balance;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} AccNo: {AccountNumber} Balance: {Balance}";
+        }
+    }
+}
