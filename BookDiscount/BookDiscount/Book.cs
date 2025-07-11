e783544b5e01c7d5b52597ec67b3ad2551392d51
unknown <youbraj.singh>
2025-07-11 16:44:09 +0530

    Assignment Completed!

---
+++ BookDiscount/BookDiscount/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookDiscount
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public bool IsBestSeller { get; set; }
+        public double DiscountedPrice { get; set; }
+
+        public Book(string bookTitle, string bookAuthor, double bookPrice, bool bookBestSeller)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            Price = bookPrice;
+            IsBestSeller = bookBestSeller;
+
+        }
+
+        public void calculateDiscountedPrice()
+        {
+            double bestSellerDiscount = 0;
+            double priceDiscount = 0;
+
+            if (IsBestSeller)
+            {
+                bestSellerDiscount = Price * 0.1;
+            }
+
+            if (Price > 500)
+            {
+                priceDiscount = Price * 0.05;
+            }
+
+            DiscountedPrice = Price - bestSellerDiscount - priceDiscount;
+        }
+
+        public void printDiscountedPrice()
+        {
+            Console.WriteLine($"Discounted price of \"{Title}\": {DiscountedPrice}");
+        }
+    }
+}
---
+++ BookDiscount/BookDiscount/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BookDiscount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Psychology of Money", "Morgan", 900, false);
+            book1.calculateDiscountedPrice();
+            book1.printDiscountedPrice();
+
+            Book book2 = new Book("Do Nothing", "Rushabh", 690, true);
+            book2.calculateDiscountedPrice();
+            book2.printDiscountedPrice();
+        }
+    }
+}
