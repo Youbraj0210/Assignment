f868a061e8065926f1cc3429876face8c61e899d
unknown <youbraj.singh>
2025-07-11 16:16:45 +0530

    Assignment Completed!

---
+++ StringPalindrome/StringPalindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace StringPalindrome
+{
+    internal class Program
+    {
+        public static bool IsPalindrome(string str)
+        {
+
+            char[] chars = str.ToCharArray();
+            Array.Reverse(chars);
+            string reversed = new string(chars);
+
+            if(reversed == str)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("is plaindrome: "+ IsPalindrome("hello"));
+        }
+    }
+}
