a46496b364be5ede390d18e9f1d022e1078f3889
unknown <youbraj.singh>
2025-07-11 16:48:40 +0530

    Assignment COmpleted!

---
+++ CountWords/CountWords/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CountWords
+{
+    internal class Program
+    {
+        public static void Count()
+        {
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+
+            // Split the sentence by whitespace, and remove empty entries
+            string[] words = sentence.Split(' ');
+
+            Console.WriteLine($"Number of words: {words.Length}");
+        }
+        static void Main(string[] args)
+        {
+            Count();
+        }
+    }
+}
