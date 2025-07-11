924c485170546b3523de7baba9abcceed194200e
unknown <youbraj.singh>
2025-07-11 15:53:17 +0530

    Assignment Completed!

---
+++ FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FibonacciSeries
+{
+    internal class Program
+    {
+        public static void fibo(int count)
+        {
+            int currNumber = 0, nextNumber = 1;
+
+            Console.Write(currNumber + " " + nextNumber + " ");
+            do
+            {
+                int temp = currNumber + nextNumber;
+                currNumber = nextNumber;
+                nextNumber = temp;
+                Console.Write(temp + " ");
+                count--;
+
+            } while (count != 0);
+        }
+        static void Main(string[] args)
+        {
+            fibo(10);
+        }
+    }
+}
